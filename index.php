02600ee8f91711b43851516500ae107b42bab037
Roy Keur <532745@student.inholland.nl>
2016-10-08 17:13:19 +0200

    JSONbestandje, als ie deze tenminste vinden wil

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $client = new Client([
     'timeout'  => 10.0,
 ]);
 
-$response = $client->get('http://crassus-php.azurewebsites.net/blabla.jsonZ');
+$response = $client->get('http://crassus-php.azurewebsites.net/blabla.json');
 
 $code = $response->getStatusCode(); // kan 200 zijn
 $reason = $response->getReasonPhrase(); // OK
