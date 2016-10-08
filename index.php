e4e93cd4a079f79a050ebc0c0a57776838d19f53
Roy Keur <532745@student.inholland.nl>
2016-10-08 16:39:26 +0200

    jongens, nog een koekje: een get request get rekt

--- index.php
+++ index.php
@@ -5,5 +5,15 @@
  * Date: 8-10-2016
  * Time: 16:01
  */
-echo "Koekjes!";
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+echo "Gompes!";
+// Create a client with a base URI
+$client = new GuzzleHttp\Client(['base_uri' => 'http://crassus-php.azurewebsites.net/']);
+// Send a request to https://foo.com/api/test
+$response = $client->request('GET', 'test');
+// Send a request to https://foo.com/root
+//$response = $client->request('GET', '/root');
 ?>
