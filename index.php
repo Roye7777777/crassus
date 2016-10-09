f382846231058ad3d9d78c6063304e58e085bc07
Roy Keur <532745@student.inholland.nl>
2016-10-09 12:20:38 +0200

    English, do you speak it?

--- index.php
+++ index.php
@@ -6,60 +6,60 @@
  * Time: 16:01
  */
 require 'vendor/autoload.php';
-
+// This is the only 'use' we use, to make the HTTP-requests possible
 use GuzzleHttp\Client;
 
-$tekst = "Een tekst met pseuso-json. Hebben we resultaat?<br/>";
-
+// Part 1: check if these HTTP-requests work, we perform a GET.
+$tekst = "We gaan hier kijken of de get-request op het pseudojson.html-bestandje werkt.<br/>";
 echo $tekst;
-// Create a client with a base URI
+
+// Create client with Base-URI in it (referring to our cloud environment)
 $client = new Client([
     // Base URI is used with relative requests
     'base_uri' => 'http://crassus-php.azurewebsites.net',
-    // You can set any number of default request options.
+    // You can set any number of default request options - since the cloud responds a bit shaggy sometimes, I put 10.0.
     'timeout'  => 10.0,
 ]);
 
+// GET: that pseudojson.html file
 $response = $client->get('http://crassus-php.azurewebsites.net/meta/pseudojson.html');
 
-$code = $response->getStatusCode(); // kan 200 zijn
-$reason = $response->getReasonPhrase(); // OK
+$code = $response->getStatusCode(); // Can be 200
+$reason = $response->getReasonPhrase(); // Can be OK
 
+// An echo with success or not
 if ($response->hasHeader('Content-Length')) {
-    echo "Ja<br/>Code:",$code,"<br/>Reason:",$reason;
+    echo "SUCCESS. <br/>Code:",$code,"<br/>Reason:",$reason;
 } else {
-    echo "Nee</br>Code:\",$code,\"<br/>Reason:\",$reason";
+    echo "FAILURE. </br>Code:\",$code,\"<br/>Reason:\",$reason";
 }
 
-echo "<br/>jongeTOCH<br/>";
-
-// "localhost", 27000 is een alternatief
+// Part 2: check if we can read from the database. Use the MongoDB-credentials as provided here to connect
 $client = new MongoDB\Client(
     'mongodb://crassus:0ur0b0r0s@ds046939.mlab.com:46939/crassus'
 );
-$collection = $client->crassus->questions;
+$dbname = 'crassus';
+$collname = 'questions';
 
+// Get the collection to read from.
+$collection = $client->$dbname->$collname;
+
+// Set up cursor (i.e. kind of like a query): find every document where question_nr is 1
 $result = $collection->find( [ 'question_nr' => 1 ] );
 
+// Show all found results (documents)
 foreach ($result as $entry) {
     echo $entry['_id'], ': ', $entry['question_nr'], "\n";
 }
 
-echo "JONGE";
-//$client = new MongoClient("mongodb://crassus:0ur0b0r0s@ds046939.mlab.com:46939"); //MongoDB\Client?
-//$collection = $client->crassus->questions;
-//$cursor = $collection->find( [ 'question_nr' => 1 ] );
-//foreach ($cursor as $doc) {
-//    echo "bla";
-//    echo $doc,"<br/>";
-//}
-///*$cursor = $collection->insertOne(
+// This might be someting for inserting something. Not tested yet.
+//
+//$cursor = $collection->insertOne(
 //    [
 //        'question_nr' => 99,
 //        'question' => 'Does this work?'
 //    ]
-//);*/
-////echo "Wat hebben we hier '{$cursor->getInsertedId()}'";
+//);
 //
-//echo "DAAG!<br/>";
+//echo "Wat hebben we hier '{$cursor->getInsertedId()}'";
 ?>
