9ef05ad06cf541908854590efddbc50640ee0d34
Roy Keur <532745@student.inholland.nl>
2016-10-11 12:26:45 +0200

    get this

---
+++ urlread.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roy
+ * Date: 11-10-2016
+ * Time: 12:25
+ */
+$var = $_GET["name"];
+$ver = $_GET["age"];
+echo "<h1>Hallo " . $_GET["name"] . "</h1>";
+echo "<h1>Hello " . $_GET["age"] . "</h1>";
+?>
