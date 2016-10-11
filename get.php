aef8ef3d2ffc766e994cd24d9c53400fff948411
Roy Keur <532745@student.inholland.nl>
2016-10-11 10:55:03 +0200

    get tests

---
+++ get.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roy
+ * Date: 11-10-2016
+ * Time: 10:52
+ */
+require 'vendor/autoload.php';
+// This is the only 'use' we use, to make the HTTP-requests possible
+use GuzzleHttp\Client;
+
+$client = new Client(['base_uri' => 'http://crassus-php.azurewebsites.net']);
+$response = $client->get('http://crassus-php.azurewebsites.net/getSomething.php');
+return $response;
---
+++ getSomething.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roy
+ * Date: 11-10-2016
+ * Time: 10:49
+ */
+require 'vendor/autoload.php';
+// here it's not used, but ok, to make phpstorm stop complaining
+// use GuzzleHttp\Client;
+
+$client = new MongoDB\Client('mongodb://crassus:0ur0b0r0s@ds046939.mlab.com:46939/crassus');
+$dbname = 'crassus';
+$collname = 'questions';
+
+// Get the collection to read from.
+$collection = $client->$dbname->$collname;
+
+// Set up cursor (i.e. kind of like a query): find every document where question_nr is 1
+$result = $collection->find( [ 'question_nr' => 1 ] );
+
+// Show all found results (documents), by showing document ID and question_nr
+return $result;
+?>
