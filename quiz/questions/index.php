e3b9135b9663f0909858e67ed9a802b4aa92680a
Roy Keur <532745@student.inholland.nl>
2016-10-13 11:05:48 +0200

    + information + quiz

---
+++ quiz/questions/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roy
+ * Date: 13-10-2016
+ * Time: 10:28
+ */
+require '../../vendor/autoload.php';
+use GuzzleHttp\Client;
+$client = new Client([
+    'base_uri' => 'http://crassus-php.azurewebsites.net',
+    'timeout'  => 10.0,
+]);
+$client=new MongoDB\Client('mongodb://crassus:0ur0b0r0s@ds046939.mlab.com:46939/crassus');
+$dbname='crassus';
+$collname='questions';
+$collection=$client->$dbname->$collname;
+$var = 1;
+if (!is_null($_GET['week_nr'])) {
+    $var = intval($_GET['week_nr']);
+}
+header('Content-Type:application/json;charset=utf-8');
+$result = $collection->find( [ 'week_nr' => $var ] );
+foreach ($result as $entry) {
+    //echo 'Document ID:', $entry['_id'], '<br/>Question_nr:', $entry['question_nr'], "<br/>";
+    echo json_encode($entry);
+}
+?>
