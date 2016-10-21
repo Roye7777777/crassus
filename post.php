b24ca06c4f86b7ce19dab860b7c8f8b1b0b1d05a
Roy Keur <532745@student.inholland.nl>
2016-10-21 12:34:07 +0200

    test y

--- post.php
+++ post.php
@@ -10,11 +10,9 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 
 $client = new Client(['base_uri' => 'http://crassus-php.azurewebsites.net']);
-$response = $client->request('GET', 'http://crassus-php.azurewebsites.net/quiz/questions');
-
-//$response = $client->request('POST', 'http://crassus-php.azurewebsites.net/post', [
-//    'json' => ['name' => 'jannus', 'age' => 99]
-//]);
+$response = $client->request('POST', 'http://crassus-php.azurewebsites.net/post/', [
+    'json' => ['name' => 'jannus', 'age' => 99]
+]);
 $code = $response->getStatusCode();
 echo $code;
 if ($response->hasHeader('Content-Length')) {
