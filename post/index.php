c97091398d7fc51e51b5ee642bc7cb34ad301dc4
Roy Keur <532745@student.inholland.nl>
2016-10-21 11:38:10 +0200

    asdfghjkl;

--- post/index.php
+++ post/index.php
@@ -1,33 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Roy
- * Date: 13-10-2016
- * Time: 10:31
- */
+
 require '../vendor/autoload.php';
+use GuzzleHttp\Client;
+$client = new Client([
+    'base_uri' => 'http://crassus-php.azurewebsites.net',
+    'timeout'  => 10.0,
+]);
+$client=new MongoDB\Client('mongodb://crassus:0ur0b0r0s@ds046939.mlab.com:46939/crassus');
+$dbname='crassus';
+$collname='users';
+$collection=$client->$dbname->$collname;
+header('Content-Type:application/json;charset=utf-8');
+$query = array();
 
-echo $_POST;
-echo $_POST['name'];
-echo $_POST['age'];
-echo '<br/>';
-echo $_GET;
-echo $_GET['name'];
-echo $_GET['age'];
+if( $_POST["name"] || $_POST["age"] ) {
+    if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
+        die ("invalid name and name should be alpha");
+    }
+//    echo "Welcome ". $_POST['name']. "<br />";
+//    echo "You are ". $_POST['age']. " years old.";
 
-//if (is_null($_POST['name']) || is_null($_POST['age'])) {
-//    echo 'Not all variables specified';
-//    return 'FAILED';
-//} else {
-    /*$client = new MongoDB\Client('mongodb://crassus:0ur0b0r0s@ds046939.mlab.com:46939/crassus');
-    $dbname = 'crassus';
-    $collname = 'users';
-    $collection = $client->$dbname->$collname;
-    //header('Content-Type:application/json;charset=utf-8');
-    $query = array('name' => $_POST['name'], 'age' => $_POST['age']);
+    $query = array( 'name' => $_POST['name'], 'age' => $_POST['age'] );
 
-    $insertOne = $collection->insertOne($query);
+    $cursor = $collection->insertOne( $query );
 
-    printf("Inserted %d document(s)\n", $insertOne->getInsertedCount());
-    var_dump($insertOne->getInsertedId());
-}*/
+    //exit();
+}
+?>
+
+
+<html>
+<body>
+
+<form action = "<?php $_PHP_SELF ?>" method = "POST">
+    Name: <input type = "text" name = "name" />
+    Age: <input type = "text" name = "age" />
+    <input type = "submit" />
+</form>
+
+</body>
+</html>
