2a96aa64ec7bff1556f6ec4e1758acca74e99ad6
Roy Keur <532745@student.inholland.nl>
2016-10-21 12:30:18 +0200

    duh

--- post.php
+++ post.php
@@ -5,6 +5,11 @@
  * Date: 21-10-2016
  * Time: 12:23
  */
+require 'vendor/autoload.php';
+// This is the only 'use' we use, to make the HTTP-requests possible
+use GuzzleHttp\Client;
+
+$client = new Client(['base_uri' => 'http://crassus-php.azurewebsites.net']);
 $response = $client->request('POST', 'http://crassus-php.azurewebsites.net/post', [
     'json' => ['name' => 'jannus', 'age' => 99]
 ]);
