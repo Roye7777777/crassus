93e638e276c27ccc08fb9346e8f34c6ee14023b7
Martijn Beljaards <martijn_beljaards@hotmail.com>
2016-10-22 20:28:07 +0200

    add: fd_post.php (food diary post) food_diary/index.php -> form action -> fd_post.php

---
+++ food_diary/fd_post.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martijnbeljaards
+ * Date: 11-10-16
+ * Time: 13:12
+ */
+
+// fd_post == food_diary_post
+require '../vendor/autoload.php';
+// This is the only 'use' we use, to make the HTTP-requests possible
+use GuzzleHttp\Client;
+
+$client = new Client(['base-uri' => 'http://crassus-php.azurewebsites.net']);
+$response = $client->request('POST', 'http://crassus-php.azurewebsites.net/food_diary/', [
+    'json' => ['breakfast' => $_POST['breakfast'], 'lunch' => $_POST['lunch'],
+        'dinner' => $_POST['dinner'], 'snacks' => $_POST['snacks']]
+]);
+?>
