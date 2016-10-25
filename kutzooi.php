eeeb35bbb799f0ef07387756ab75e8dbcc169eb4
Roye7777777 <roypret@live.nl>
2016-10-25 16:48:43 +0200

    waardeloos dit

--- kutzooi.php
+++ kutzooi.php
@@ -4,33 +4,27 @@ require 'db.php';
 $collname='users';
 $collection=$dbclient->$dbname->$collname;
 header('Content-Type:application/json;charset=utf-8');
-echo 'fdgdgg';
 
 if (!isset($_GET['id']) || is_null($_GET['id']))
     die ("No ID given");
 
+// Ik begrijp hier helemaal NIETS van. Geen enkele keer werkt de $collection->find(array("_id"=>MongoDB\BSON\ObjectID($_GET['id']))... overal crasht het
 $cursor = $collection->find();
 $check = false;
+$i = 0;
+$return = [];
 foreach($cursor as $item) {
-    echo($item['_id']);
-    echo(new MongoDB\BSON\ObjectId($_GET['id']));
     if (new MongoDB\BSON\ObjectId($_GET['id']) == $item['_id']) {
         $check = true;
-        break;
+        $return[$i] = array(
+            '_id'=>utf8_encode($item['_id'])
+        );
+        $i++;
     }
 }
 if (!$check)
     die ("No results");
 
-$i = 0;
-echo json_encode(array('test'=>'df'));
-$return = [];
-foreach($cursor as $item){
-    $return[$i] = array(
-        '_id'=>utf8_encode($item['_id'])
-    );
-    $i++;
-}
 echo json_encode($return, JSON_FORCE_OBJECT);
 
 /*$data = json_decode(file_get_contents('php://input'), true);
