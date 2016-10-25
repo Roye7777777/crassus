1afecb0f5597bc1f5fad3f698270462168745f9d
Martijn Beljaards <martijn_beljaards@hotmail.com>
2016-10-25 13:33:40 +0200

    empty default test

--- post/index.php
+++ post/index.php
@@ -13,25 +13,37 @@ $weight = $data["weight"];
 $gender = $data["gender"];
 $length = $data["length"];
 
-
-if( empty($name) || empty($age) ) {
-    echo 'No arguments given';
-} else {
-    if (preg_match("/[^A-Za-z'-]/",$name )) {
-        die ("invalid name and name should be alpha");
+function default_value(&$var, $default)
+{
+    if (empty($var))
+    {
+        $var = $default;
     }
-    //header('Content-Type:application/json;charset=utf-8');
-    $query = array( 'name' => $name, 'age' => $age, 'weight' => $weight, 'length' => $length, 'gender' => $gender );
-    $cursor = $collection->insertOne( $query );
-
-    echo "Welcome ". $name. "<br />";
-    echo "You are ". $age. " years old.<br/>";
-    echo $weight. "<br />";
-    echo $length. "<br />";
-    echo $gender. "<br />";
-    //echo "Inserted %d document(s)<br/>", $cursor->getInsertedCount();
-    //echo $cursor->getInsertedId();
-
-    exit();
 }
+default_value($name, "");
+default_value($age, "");
+default_value($weight, "");
+default_value($gender, "");
+default_value($length, "");
+
+if (preg_match("/[^A-Za-z'-]/",$name )) {
+    die ("invalid name and name should be alpha");
+}
+//header('Content-Type:application/json;charset=utf-8');
+$query = array( 'name' => $name, 'age' => $age, 'weight' => $weight, 'length' => $length, 'gender' => $gender );
+$cursor = $collection->insertOne( $query );
+
+echo "Welcome ". $name. "<br />";
+echo "You are ". $age. " years old.<br/>";
+echo $weight. "<br />";
+echo $length. "<br />";
+echo $gender. "<br />";
+//echo "Inserted %d document(s)<br/>", $cursor->getInsertedCount();
+//echo $cursor->getInsertedId();
+
+exit();
+
+//    echo json_encode(array("success"=>1), JSON_FORCE_OBJECT);
+
+
 ?>
