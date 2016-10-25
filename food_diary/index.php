feb1db3c0d82984fc68afb9e2ba476085349504c
Roye7777777 <roypret@live.nl>
2016-10-25 17:43:48 +0200

    query var dump2

--- food_diary/index.php
+++ food_diary/index.php
@@ -14,6 +14,8 @@ header('Content-Type:application/json;charset=utf-8');
 $query = array();
 $foodList = array();
 
+var_dump($query = array('food_diaries' => array('$elemMatch'=> array('breakfast'=>'muesli','lunch'=>'kaas'))));
+
 if (!empty($_GET)) {
     if (!is_null($_GET['breakfast']))
         array_push($foodList, array('breakfast' => str_replace(array('_', ','), array(' ', ''), $_GET['breakfast'])));
