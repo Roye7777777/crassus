319d4bba20253db723a2bdb6f32d50da9965c2a5
emielwit <emielwogmeer@gmail.com>
2016-10-26 12:46:08 +0200

    ?

--- users/index.php
+++ users/index.php
@@ -25,7 +25,7 @@ if (isset($_GET['id'])) {
         }
     }
     if (!$check) {
-        var_dump('kaas');
+        die('No results found');
     }
 } else {
     foreach ($collection->find() as $item) {
