d6bcc2388b22c10c366220d1a578554b3814ca43
Job <jobdeboer87@gmail.com>
2016-10-27 12:39:00 +0200

    usars

--- test/index.php
+++ test/index.php
@@ -6,10 +6,10 @@ use GuzzleHttp\Exception\RequestException;
 $promise = $apiclient->requestAsync('GET', '/users/');
 $promise->then(
     function (ResponseInterface $res) {
-        echo $res->getStatusCode() . "\n";
+        echo $res->getStatusCode() . "\n aa";
     },
     function (RequestException $e) {
-        echo $e->getMessage() . "\n";
+        echo $e->getMessage() . "\n aa";
         echo $e->getRequest()->getMethod();
     }
 );
