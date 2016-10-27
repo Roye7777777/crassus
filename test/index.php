abb1cdf82ab74164a82113e7d84f3691106754a5
Job <jobdeboer87@gmail.com>
2016-10-27 12:24:59 +0200

    doe meer

--- test/index.php
+++ test/index.php
@@ -6,6 +6,7 @@ require '../db.php';
 // dit moet je even nakijken, of het op deze manier gaat, dat je een json eraan meegeeft
 
 $response = $apiclient->request('GET', '/users/');
+$response2 = $apiclient->request('POST', '/users/');
 //$response = $apiclient->get('/users/');
 
 $code = $response->getStatusCode();
@@ -18,6 +19,18 @@ echo json_encode(array(
     'body'=>$body,
     'len'=>$leng,
     'type'=>$type
+));
+
+$code2 = $response2->getStatusCode();
+$body2 = $response2->getBody();
+$leng2 = $response2->getHeader('Content-Length');
+$type2 = $response2->getHeader('Content-Type');
+
+echo json_encode(array(
+    'Code'=>$code2,
+    'body'=>$body2,
+    'len'=>$leng2,
+    'type'=>$type2
 ));
 /*
 $promise = $apiclient->requestAsync('GET', 'http://httpbin.org/get');
