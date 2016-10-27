560825ca135c711cd91cdf92c0abdb372ae5fe65
emielwit <emielwogmeer@gmail.com>
2016-10-27 12:44:50 +0200

    Update user post with username/password

--- post/index.php
+++ post/index.php
@@ -5,7 +5,10 @@ $collection=$dbclient->$dbname->$collname;
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-$name = $data["name"];
+$username = $data["username"];
+$password = $data["password"];
+$first_name = $data["first_name"];
+$last_name = $data["last_name"];
 $age = $data["age"];
 $weight = $data["weight"];
 $gender = $data["gender"];
@@ -18,20 +21,24 @@ function default_value(&$var, $default)
         $var = $default;
     }
 }
-default_value($name, "");
+
+default_value($username, "");
+default_value($password, "");
+default_value($last_name, "");
+default_value($first_name, "");
 default_value($age, "");
 default_value($weight, "");
 default_value($gender, "");
 default_value($length, "");
 
-if (preg_match("/[^A-Za-z'-]/",$name )) {
+if (preg_match("/[^A-Za-z'-]/",$first_name, $last_name )) {
     die ("invalid name and name should be alpha");
 }
 //header('Content-Type:application/json;charset=utf-8');
-$query = array( 'name' => $name, 'age' => $age, 'weight' => $weight, 'length' => $length, 'gender' => $gender );
+$query = array( 'password' => $password, 'username' => $username, 'last_name' => $last_name, 'first_name' => $first_name, 'age' => $age, 'weight' => $weight, 'length' => $length, 'gender' => $gender );
 $cursor = $collection->insertOne( $query );
 
-echo "Welcome ". $name. "<br />";
+echo "Welcome ". $first_name. "<br />";
 echo "You are ". $age. " years old.<br/>";
 echo $weight. "<br />";
 echo $length. "<br />";
